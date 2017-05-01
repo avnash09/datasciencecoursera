bb83e5bf74cb5ad177b48380c9e9fd69786042f4
Avinash <avinash.rkl09@gmail.com>
2017-05-01 05:37:46 +0530

    R Programming week 2 part 2

---
+++ complete.R
@@ -0,0 +1,21 @@
+complete <- function(directory, id = 1:332) {
+  
+  len <- length(id)
+  cnt <- 1
+  x <- numeric(len)
+  y <- numeric(len)
+  for(i in id) {
+    if(i >=1 && i <=9) {
+      j<- paste("00",i,sep = "")
+    } else if (i >=10 && i <= 99) {
+      j<- paste("0",i,sep = "")
+    }
+    f <- paste(file.path(directory,j),"csv",sep = ".")
+    a <- read.csv(f)
+    x[cnt] <- i
+    y[cnt] <- nrow(na.omit(a))
+    cnt <- cnt + 1
+  }
+  data <- data.frame(id=x, nobs=y)
+  data
+}
